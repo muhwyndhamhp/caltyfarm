d92c0db0a66d0c45d0ba0d9130c9ff31ad868e42
muhwyndhamhp <wyndham.haryata.p@mail.ugm.ac.id>
2018-11-26 18:19:08 +0700

    add method to check user is logged in or not

--- app/src/main/java/com/caltyfarm/caltyfarm/ui/Splash.kt
+++ app/src/main/java/com/caltyfarm/caltyfarm/ui/Splash.kt
@@ -1,13 +1,32 @@
 package com.caltyfarm.caltyfarm.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.caltyfarm.caltyfarm.R
+import com.google.firebase.auth.FirebaseAuth
 
 class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        checkUserLogin()
+    }
+
+    private fun checkUserLogin() {
+        if(FirebaseAuth.getInstance().currentUser == null){
+            navigateToRegister()
+        } else {
+            navigateToMain()
+        }
+    }
+
+    private fun navigateToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun navigateToRegister() {
+        startActivity(Intent(this, AuthActivity::class.java))
     }
 }
