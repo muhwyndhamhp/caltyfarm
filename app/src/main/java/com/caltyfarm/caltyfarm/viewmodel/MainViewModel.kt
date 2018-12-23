6135c347c6d535c59ddd3e1251d4f5f97fcac304
muhwyndhamhp <wyndham.haryata.p@mail.ugm.ac.id>
2018-12-23 20:44:47 +0700

    add function to retrieve user data from repository

--- app/src/main/java/com/caltyfarm/caltyfarm/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/caltyfarm/caltyfarm/viewmodel/MainViewModel.kt
@@ -4,12 +4,30 @@ import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.caltyfarm.caltyfarm.data.AppRepository
+import com.caltyfarm.caltyfarm.data.model.User
+import com.google.firebase.auth.FirebaseAuth
 
-class MainViewModel(val context: Context, val appRepository: AppRepository): ViewModel(){
+class MainViewModel(val context: Context, val appRepository: AppRepository) : ViewModel() {
 
-    val fragmentPosition = MutableLiveData<Int>()
+    val userData = MutableLiveData<User>()
 
-    fun setFragmentPosition(position: Int) {
-        fragmentPosition.value = position
+    var errorMessages :MutableLiveData<String> = MutableLiveData()
+
+    init {
+        appRepository.getUserData(
+            FirebaseAuth.getInstance().currentUser!!.uid,
+            object : AppRepository.OnUserDataCallback {
+                override fun onDataRetrieved(user: User?) {
+                    if(user != null){
+                        userData.value = user
+                    }
+                }
+
+                override fun onFailed(exception: Exception) {
+                    errorMessages.value = exception.message!!
+                }
+
+            })
     }
+
 }
